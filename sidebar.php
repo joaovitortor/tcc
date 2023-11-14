34308740cdd057f5e0dc037bfe17350f8d3035fa
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2023-11-14 14:32:10 -0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,51 +1,51 @@
 <li><a class="emprestimo-btn">
-        <i class="fa-regular fa-user"></i><span class="link-name">Emprestimo</span></a>
+        <i class="fa-solid fa-arrows-left-right-to-line"></i><span class="link-name">Emprestimo</span></a>
     <ul class="emprestimo-show">
-        <li><a href="">Cadastrar</a></li>
-        <li><a href="">Listar</a></li>
-        <li><a href="">Devolução</a></li>
+        <li><a href="" class="text">Cadastrar</a></li>
+        <li><a href="" class="text">Listar</a></li>
+        <li><a href="" class="text">Devolução</a></li>
     </ul>
 </li>
 <li><a class="leitor-btn">
-        <i class="fa-regular fa-user"></i><span class="link-name">Leitor</span></a>
+        <i class="fa-solid fa-user"></i><span class="link-name">Leitor</span></a>
     <ul class="leitor-show">
-        <li><a href="cadastrarLeitor.php">Cadastrar</a></li>
-        <li><a href="listarLeitor.php">Listar</a></li>
+        <li><a href="cadastrarLeitor.php" class="text">Cadastrar</a></li>
+        <li><a href="listarLeitor.php" class="text">Listar</a></li>
     </ul>
 </li>
 <li><a class="livro-btn">
-        <i class="fa-solid fa-book"></i></i><span class="link-name">Livro</span></a>
+        <i class="fa-solid fa-book"></i><span class="link-name">Livro</span></a>
     <ul class="livro-show">
-        <li><a href="cadastrarLivro.php">Cadastrar</a></li>
-        <li><a href="listarLivros.php">Listar</a></li>
+        <li><a href="cadastrarLivro.php" class="text">Cadastrar</a></li>
+        <li><a href="listarLivros.php" class="text">Listar</a></li>
     </ul>
 </li>
 <li><a class="autor-btn">
-        <i class="fa-regular fa-user"></i><span class="link-name">Autor</span></a>
+        <i class="fa-solid fa-pen-nib"></i><span class="link-name">Autor</span></a>
     <ul class="autor-show">
-        <li><a href="cadastrarAutor.php">Cadastrar</a></li>
-        <li><a href="listarAutores.php">Listar</a></li>
+        <li><a href="cadastrarAutor.php" class="text">Cadastrar</a></li>
+        <li><a href="listarAutores.php" class="text">Listar</a></li>
     </ul>
 </li>
 <li><a class="administrador-btn">
-        <i class="fa-regular fa-user"></i><span class="link-name">Administrador</span></a>
+        <i class="fa-solid fa-user-lock"></i><span class="link-name">Administrador</span></a>
     <ul class="administrador-show">
-        <li><a href="cadastrarAdministrador.php">Cadastrar</a></li>
-        <li><a href="listarAdministrador.php">Listar</a></li>
+        <li><a href="cadastrarAdministrador.php" class="text">Cadastrar</a></li>
+        <li><a href="listarAdministrador.php" class="text">Listar</a></li>
     </ul>
 </li>
 <li><a class="genero-btn">
-        <i class="fa-regular fa-user"></i><span class="link-name">Genero</span></a>
+        <i class="fa-solid fa-wand-sparkles"></i><span class="link-name">Genero</span></a>
     <ul class="genero-show">
-        <li><a href="cadastrarGenero.php">Cadastrar</a></li>
-        <li><a href="listarGenero.php">Listar</a></li>
+        <li><a href="cadastrarGenero.php" class="text">Cadastrar</a></li>
+        <li><a href="listarGenero.php" class="text">Listar</a></li>
     </ul>
 </li>
 <li><a class="editora-btn">
-        <i class="fa-regular fa-user"></i><span class="link-name">Editora</span></a>
+        <i class="fa-solid fa-newspaper"></i><span class="link-name">Editora</span></a>
     <ul class="editora-show">
-        <li><a href="cadastrarEditora.php">Cadastrar</a></li>
-        <li><a href="listarEditora.php">Listar</a></li>
+        <li><a href="cadastrarEditora.php" class="text">Cadastrar</a></li>
+        <li><a href="listarEditora.php" class="text">Listar</a></li>
     </ul>
 </li>
 
