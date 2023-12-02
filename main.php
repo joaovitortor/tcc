2e3afe77fb2e32fa75f543006c4910cead883f87
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2023-12-02 16:00:22 -0300

    Update main.php

--- main.php
+++ main.php
@@ -8,25 +8,26 @@ require_once("navbar.php");
 
     </div>
     <div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2 class="modal-title fs-5" id="exampleModalLabel">Informações do Usuário
-                </h2>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h2 class="modal-title fs-5" id="exampleModalLabel">Procurar Empréstimo
+                    </h2>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form method="post" action="procurarEmprestimo.php">
+                    <div class="modal-body">
+                        <label for="">Código do Empréstimo:
+                        </label>
+                        <input class="geekcb-field" placeholder="Código" required type="texto" name="idEmprestimo">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        <button type="submit" name="procurarEmprestimo" class="btn btn-danger"
+                            data-bs-dismiss="modal">Procurar</button>
+                    </div>
+                </form>
             </div>
-            <form method="post" action="procurarEmprestimo.php">
-                <div class="modal-body">
-                    <label for="">Código do Empréstimo:
-                    </label>
-                    <input class="geekcb-field" placeholder="Código" required type="texto" name="idEmprestimo">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                    <button type="submit" name="procurarEmprestimo" class="btn btn-danger" data-bs-dismiss="modal">Procurar</button>
-                </div>
-            </form>
         </div>
     </div>
-</div>
     <?php require_once("rodape.php");
