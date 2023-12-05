7f22f83826936f4592f8c244c733cbec90229b41
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2023-12-05 14:13:10 -0300

    .

--- main.php
+++ main.php
@@ -1,11 +1,34 @@
 <?php
 require_once("admAutenticacao.php");
 require_once("navbar.php");
+
+$voltar = "";
+if (isset($_POST['pesquisar'])) {
+    $voltar = '<a href="main.php" style="text-decoration: none"><button name="voltar" stype="button" class="botaopesquisarAcervo">Voltar</button></a>';
+}
 ?>
 
 <section class="home-section">
-    <?php   
-    require_once("acervo1.php"); 
+    <br><br>
+    <h1 class="titulo text"> <img src="logobiblio.png" alt="logo" width="5%"> Bibliotech</h1>
+
+    <center>
+        <form method="post">
+            <label name="pesquisa" for="exampleFormControlInput1" class="titulo text">Pesquisar livros no acervo da
+                biblioteca</label>
+            <div class="input-button-container">
+                <input name="pesquisa" type="text" class="formcampo">
+                <button name="pesquisar" stype="button" class="botaopesquisarAcervo"><i
+                        class="fa-solid fa-magnifying-glass"></i>
+                </button>
+                <?= $voltar; ?>
+            </div>
+            <br><br>
+        </form>
+    </center>
+
+    <?php
+    require_once("acervo1.php");
     require_once("procurarEmprestimo.php");
     ?>
 
